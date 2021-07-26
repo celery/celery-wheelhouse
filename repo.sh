5f7a511402d7f0a2511ca6b46d75422ee5ed5dea
Thomas Grainger <tagrain@gmail.com>
2021-07-26 11:38:15 +0100

    fix path

--- repo.sh
+++ repo.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-dumb-pypi --package-list=<(find files -name '*.whl' -type f -printf '%f\n') --packages-url="/celery-wheelhouse/_files" --output-dir repo
+dumb-pypi --package-list=<(find files -name '*.whl' -type f -printf '%f\n') --packages-url="/celery-wheelhouse/files" --output-dir repo
